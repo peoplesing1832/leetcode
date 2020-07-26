4419357dbcfedfe4dd1d5203b636aca9cc7d0874
peoplesing1832 <peoplesing1832@gmail.com>
2020-07-26 21:44:25 +0800

    feat: 无向图，adjacencyList实现

---
+++ 图/AdjacencyList.js
@@ -0,0 +1,39 @@
+/**
+ * 无向图
+ */
+class Graph {
+    constructor (noOfNode) {
+        // key ～ 节点
+        // value ～ 相邻节点的列表
+        this.adjacencyList = new Map();
+        // 节点的数量
+        this.noOfVertices = noOfNode;
+    }
+
+    /**
+     * 添加节点
+     */
+    addNode (node) {
+        this.adjacencyList.set(node, []);
+    }
+
+    /**
+     * 添加边
+     * to 节点1
+     * form 节点2
+     */
+    addEdge (to, from) {
+        // 无向图，所以边是连接两边的
+        this.adjacencyList.get(to).push(from);
+        this.adjacencyList.get(from).push(to);
+    }
+
+    print () {
+        const nodes = this.adjacencyList.keys();
+        for (let node of nodes) {
+            const neighbors = this.adjacencyList.get(node);
+            const neighborsString = neighbors.join(' ');
+            console.log(`节点${node}, 相邻节点${neighborsString}`);
+        }
+    }
+}

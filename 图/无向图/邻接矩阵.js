6bc16ef65414b7d8445c1608be733210317f565c
peoplesing1832 <peoplesing1832@gmail.com>
2020-07-30 01:00:15 +0800

    feat: 无向图，bfs，dfs

--- 图/无向图/邻接矩阵.js
+++ 图/无向图/邻接矩阵.js
@@ -52,5 +52,74 @@ class Graph {
             console.log(`节点${current}, 相邻节点${neighborsString}`);
         }
     }
+
+    bfs (node) {
+        if (node >= 0 && node < this.matrix.length) {
+            const result = [];
+            const hash = {};
+            const queue = [];
+            for (let i = 0; i < this.matrix.length; i++) {
+                hash[i] = false;
+            }
+            hash[node] = true;
+            result.push(node);
+            queue.push(node);
+            while(queue.length > 0) {
+                let q = queue.shift();
+                let list = this.matrix[q];
+                for (let i = 0; i < list.length; i++) {
+                    if (
+                        list[i] === 1 &&
+                        !hash[i]
+                    ) {
+                        result.push(i);
+                        hash[i] = true;
+                        queue.push(i);
+                    }
+                }
+            }
+            return result;
+        }
+        return -1;
+    }
+
+    dfs (node) {
+        if (node >= 0 && node < this.matrix.length) {
+            const result = [];
+            const hash = {};
+            for (let i = 0; i < this.matrix.length; i++) {
+                hash[i] = false;
+            }
+            hash[node] = true;
+            result.push(node);
+            const DFS = (node) => {
+                let list = this.matrix[node];
+                for (let i = 0; i < list.length; i++) {
+                    if (
+                        list[i] === 1 &&
+                        !hash[i]
+                    ) {
+                        result.push(i);
+                        hash[i] = true;
+                        DFS(i);
+                    }
+                }
+            };
+            DFS(node);
+            return result;
+        }
+        return -1;
+    }
 }
 
+let graph = new Graph();
+graph.addNode();
+graph.addNode();
+graph.addNode();
+graph.addNode();
+
+graph.addEdge(0,1)
+graph.addEdge(1,2)
+graph.addEdge(0,3)
+
+console.log(graph.dfs(0))

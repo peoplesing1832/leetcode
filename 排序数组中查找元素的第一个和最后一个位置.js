5486fbd69cda47e91a961be2958d5278ead418e9
peoplesing1832 <68176628+peoplesing1832@users.noreply.github.com>
2020-12-04 14:33:04 +0800

    Update 排序数组中查找元素的第一个和最后一个位置.js

--- 排序数组中查找元素的第一个和最后一个位置.js
+++ 排序数组中查找元素的第一个和最后一个位置.js
@@ -4,45 +4,30 @@
  * @return {number[]}
  */
 var searchRange = function(nums, target) { 
-
-    const result = [];
+    let index = -1;
 
     const binarySearch = (arr, target, baseLength) => {
-        if (!arr.length) {
-            return -1;
+        if (arr.length) {
+            const len = arr.length;
+            if (len === 1 && arr[0] === target) {
+                index = baseLength + 0
+            } else if (len > 1) {
+                const centerIndex = Math.floor(len / 2);
+                const center = arr[centerIndex];
+                const left = arr.slice(0, len - centerIndex);
+                const right = arr.slice(centerIndex + 1);
+                if (target === center) {
+                    index = centerIndex + baseLength;
+                } else if (target < center) {
+                    binarySearch(left, target, 0);
+                } else if (target > center) {
+                    binarySearch(right, target, left.length);
+                } 
+            }
         }
-        const len = arr.length;
-        const centerIndex = Math.floor(len / 2);
-        const center = arr[centerIndex];
-        const left = arr.slice(0, center - 1);
-        const right = arr.slice(center);
-        if (target === center) {
-            return centerIndex + baseLength;
-        } else if (target < center) {
-            return binarySearch(left, target, 0);
-        } else if (target > center) {
-            return binarySearch(right, target, left.length + 1);
-        } 
     }
 
     // nums是已经排过序的
-    const index = binarySearch([...nums], target, 0);
+    binarySearch([...nums], target, 0);
 
-    if (index === -1) {
-        result.push(-1)
-        result.push(-1)
-    } else {
-        if (nums[index] === nums[index - 1]) {
-            result.push(index - 1)
-            result.push(index)
-        } else if (nums[index] === nums[index + 1]) {
-            result.push(index)
-            result.push(index + 1)
-        } else {
-            result.push(index)
-            result.push(-1)
-        }
-    }
-
-    return result;
 };

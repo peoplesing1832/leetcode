a2dad896fa6bf9b86fd914ff11c770eeddeb8090
peoplesing1832 <68176628+peoplesing1832@users.noreply.github.com>
2020-12-07 16:14:09 +0800

    Update 螺旋矩阵III.js

--- 螺旋矩阵III.js
+++ 螺旋矩阵III.js
@@ -9,7 +9,7 @@ var spiralMatrixIII = function(R, C, r0, c0) {
     const total = R * C
     const result = []
     let current = 0
-    let turnsNumber = 1; // 每间隔2次转弯的时候，边长需要加1（但是前3次都是2）
+    let turnsNumber = 1; // 每间隔2次转弯的时候，边长需要加1（但是前3次都是2，3次之后是这个规律）
     let direction = 'bottom' // left -> bottom -> right -> top -> left 方向变化的顺序
     let sideLength = 2;
     let x = c0;
@@ -25,6 +25,7 @@ var spiralMatrixIII = function(R, C, r0, c0) {
         }
     }
 
+    // 首先尝试添加起点的坐标
     if (isLegal()) {
         result.push([y, x])
         x = x + 1;
@@ -40,7 +41,7 @@ var spiralMatrixIII = function(R, C, r0, c0) {
     }
 
     const left = () => {
-        let num = 1;
+        let num = 0;
         while (num < sideLength) {
             if (isLegal()) {
                 result.push([y, x]);
@@ -57,7 +58,7 @@ var spiralMatrixIII = function(R, C, r0, c0) {
     }
 
     const bottom = () => {
-        let num = 1;
+        let num = 0;
         while (num < sideLength) {
             if (isLegal()) {
                 result.push([y, x]);
@@ -74,7 +75,7 @@ var spiralMatrixIII = function(R, C, r0, c0) {
     } 
 
     const right = () => {
-        let num = 1;
+        let num = 0;
         while (num < sideLength) {
             if (isLegal()) {
                 result.push([y, x]);
@@ -86,22 +87,22 @@ var spiralMatrixIII = function(R, C, r0, c0) {
             }
         }
         direction = 'top'
-        x = x + 1;
-        y = y;
+        x = x;
+        y = y - 1;
     }
 
     const top = () => {
-        x = x;
-        y = y - 1;
-        let num = 1;
+        let num = 0;
         while (num < sideLength) {
             if (isLegal()) {
+                console.log('top', y,x)
                 result.push([y, x]);
             }
             num += 1;
             if (num < sideLength) {
                 x = x;
                 y = y - 1;
+                
             }
         }
         direction = 'left'
@@ -110,7 +111,6 @@ var spiralMatrixIII = function(R, C, r0, c0) {
     }
 
     while (current < total) {
-        turnsNumber += 1
         calculateSideLength();
         switch (direction) {
             case 'left':
@@ -126,21 +126,9 @@ var spiralMatrixIII = function(R, C, r0, c0) {
                 top();
                 break;                
         }
-        console.log(result)
+        turnsNumber += 1
     }
 
 
     return result
 };
-
-console.log(
-    spiralMatrixIII(5, 6, 1, 4)
-)
-
-
-// a a a a a a a 7 8 8 8 8 8 a a 
-// a a a a a a a 7 3 4 4 4 9 a a
-// a a a a a a a 7 3 0 1 5 9 a a
-// a a a a a a a 7 2 2 1 5 9 a a
-// a a a a a a a 6 6 6 6 5 9 a a
-// a a a a a a e e e e e e 9 a a

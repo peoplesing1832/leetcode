f4cab92436287d64f083adeef12bea66ace255c7
peoplesing1832 <68176628+peoplesing1832@users.noreply.github.com>
2020-12-10 21:49:35 +0800

    Update 复原IP地址.js

--- 复原IP地址.js
+++ 复原IP地址.js
@@ -6,6 +6,9 @@ var restoreIpAddresses = function(s) {
     const result = [];
 
     const isLegal = (n) => {
+        if (n.length <= 0) {
+            return false
+        }
         if (n.length > 1 && n[0] === '0') {
             return false
         }
@@ -15,34 +18,29 @@ var restoreIpAddresses = function(s) {
         return true
     }
 
-    const fn = (head, tail, n) => {
-        // console.log(head)
-        if (n > 0) {
+    const fn = (head, tail, n) => {        
+        if (n <= 0) {
+            if (isLegal(tail)) {
+                result.push(`${head}.${tail}`)
+            }
+        } else {
             for (let i = 0; i < 3; i++) {
                 const h = tail.slice(0, i + 1);
                 if (isLegal(h)) {
+                    let newHead = ''
                     if (head.length) {
-                        head = `${head},${h}`;
+                        newHead = `${head}.${h}`;
                     } else {
-                        head = `${h}`;
+                        newHead = `${h}`;
                     }
                     const t = tail.slice(i + 1);
-                    console.log(h)
-                    console.log(head)
-                    console.log('-=========')
-                    fn(head, t, n - 1);
+                    fn(newHead, t, n - 1);
                 }
             }
-        } else if (n === 0 && isLegal(tail)) {
-            result.push(`${head},${tail}`)
         }
     };
 
-    fn('', s, 2);
-
-    console.log(result)
+    fn('', s, 3);
 
     return result;
 };
-
-restoreIpAddresses("25525511135")

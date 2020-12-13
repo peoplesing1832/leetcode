f55ef7ea38de9c8dc816f834d539d35b0aaffe96
peoplesing1832 <68176628+peoplesing1832@users.noreply.github.com>
2020-12-13 23:17:43 +0800

    Update 组合总合.js

--- 组合总合.js
+++ 组合总合.js
@@ -29,3 +29,39 @@ var combinationSum = function(candidates, target) {
 
     return result;
 };
+
+// 不使用hash
+
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function(candidates, target) {
+    const result = []
+    candidates.sort((a, b) => a - b)
+
+    const fn = (arr, sum, index) => {
+        if (sum > target) {
+            return
+        }
+        if (sum === target) {
+            result.push(arr)
+        } else {
+            // i = index, 避免重复的递归，同时避免重复
+            for (let i = index; i < candidates.length; i++) {
+                fn([...arr, candidates[i]], sum + candidates[i], i)
+            }
+        }
+    }
+
+    for (let i = 0; i < candidates.length; i++) {
+        if (candidates[i] === target) {
+            result.push([candidates[i]])
+        } else {
+            fn([candidates[i]], candidates[i], i)
+        }
+    }
+
+    return result;
+};

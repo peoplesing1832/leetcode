9414f561e50367a6fc6f5ae91b50ba7f47d251dc
peoplesing1832 <68176628+peoplesing1832@users.noreply.github.com>
2020-12-13 18:09:27 +0800

    Update 全排列II.js

--- 全排列II.js
+++ 全排列II.js
@@ -37,3 +37,43 @@ var permuteUnique = function(nums) {
 
     return result;
 };
+
+// 🚨 同层剪枝
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permuteUnique = function(nums) {
+    const result = []
+
+    nums.sort((a, b) => a - b)
+
+    const fn = (head, tail) => {
+        for (let i = 0; i < tail.length; i++) {
+            let temp = [...tail]
+            temp.splice(i, 1)
+            if (tail[i] !== tail[i - 1]) {
+                if (temp.length) {
+                    fn([...head, tail[i]], temp);
+                } else {
+                    result.push([...head, tail[i]])
+                }
+            }
+        }
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        let temp = [...nums]
+        temp.splice(i, 1)
+        if (nums[i] !== nums[i - 1]) {
+            if (temp.length) {
+                fn([nums[i]], temp);
+            } else {
+                result.push([nums[i]])
+            }
+        }
+        
+    }
+
+    return result;
+};

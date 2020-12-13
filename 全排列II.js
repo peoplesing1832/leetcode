9df5f93cfa2317e263e8cff6c2cf1ae0b926fe4c
peoplesing1832 <68176628+peoplesing1832@users.noreply.github.com>
2020-12-13 18:03:26 +0800

    Update 全排列II.js

--- 全排列II.js
+++ 全排列II.js
@@ -1,1 +1,39 @@
+// 🚨 使用hash去重
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permuteUnique = function(nums) {
+    const result = []
+    const hash = {}
 
+    nums.sort((a, b) => a - b)
+
+    const fn = (head, tail) => {
+        for (let i = 0; i < tail.length; i++) {
+            let temp = [...tail]
+            temp.splice(i, 1)
+            if (temp.length) {
+                fn([...head, tail[i]], temp);
+            } else {
+                let key = [...head, tail[i]].join(',');
+                if (!hash[key]) {
+                    result.push([...head, tail[i]])
+                    hash[key] = true
+                }
+            }
+        }
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        let temp = [...nums]
+        temp.splice(i, 1)
+        if (temp.length) {
+            fn([nums[i]], temp);
+        } else {
+            result.push([nums[i]])
+        }
+    }
+
+    return result;
+};

0fd38ba4f7a3b74c43c57b041357e10f7de55d89
peoplesing1832 <68176628+peoplesing1832@users.noreply.github.com>
2020-12-13 12:15:22 +0800

    Update 分割数组为连续子序列.js

--- 分割数组为连续子序列.js
+++ 分割数组为连续子序列.js
@@ -1,1 +1,37 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var isPossible = function(nums) {
+    const num = {};
+    const tail = {};
 
+    for (let i = 0; i < nums.length; i++) {
+       if (!num[nums[i]]) {
+           num[nums[i]] = 1;
+           tail[nums[i]] = 0;
+       } else {
+           num[nums[i]] += 1;
+       }
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        let key = nums[i]
+        if (num[key] === 0) {
+            continue
+        } else if (num[key] > 0 && tail[key - 1] > 0) {
+            num[key] -= 1
+            tail[key - 1] -= 1
+            tail[key] += 1
+        } else if (num[key] > 0 && num[key + 1] > 0 && num[key + 2] > 0) {
+            num[key] -= 1
+            num[key + 1] -= 1
+            num[key + 2] -= 1
+            tail[key + 2] += 1
+        } else {
+            return false
+        }
+    }
+    
+    return true
+};

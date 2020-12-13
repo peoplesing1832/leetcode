269f7de6c80ff86882c65d0604a4748f4c2c4835
peoplesing1832 <68176628+peoplesing1832@users.noreply.github.com>
2020-12-13 17:59:11 +0800

    Update 全排列.js

--- 全排列.js
+++ 全排列.js
@@ -1,1 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+    const result = []
 
+    const fn = (head, tail) => {
+        for (let i = 0; i < tail.length; i++) {
+            let temp = [...tail]
+            temp.splice(i, 1)
+            if (temp.length) {
+                fn([...head, tail[i]], temp);
+            } else {
+                result.push([...head, tail[i]])
+            }
+        }
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        let temp = [...nums]
+        temp.splice(i, 1)
+        if (temp.length) {
+            fn([nums[i]], temp);
+        } else {
+            result.push([nums[i]])
+        }
+    }
+
+    return result;
+};

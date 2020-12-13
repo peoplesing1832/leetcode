ab5a5ec511a449aed21a1e36f83f29bf4107cd02
peoplesing1832 <68176628+peoplesing1832@users.noreply.github.com>
2020-12-13 17:35:55 +0800

    Update 子集 II.js

--- 子集 II.js
+++ 子集 II.js
@@ -38,3 +38,42 @@ var subsetsWithDup = function(nums) {
 
     return result;
 };
+
+// 🚨 同层剪枝
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsetsWithDup = function(nums) {
+    const result = []
+
+    result.push([])
+
+    nums.sort((a, b) => a - b)
+
+    const fn = (head, tail) => {
+        for (let i = 0; i < tail.length; i++) {
+            if (tail[i] === tail[i - 1]) {
+                continue
+            } else {
+                result.push([...head, tail[i]])
+                if (tail.slice(i + 1)) {
+                    fn([...head, tail[i]], tail.slice(i + 1))
+                }
+            }
+        }
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === nums[i - 1]) {
+            continue
+        } else {
+            result.push([nums[i]])
+            if (nums.slice(i + 1).length) {
+                fn([nums[i]], nums.slice(i + 1))
+            }
+        }  
+    }
+
+    return result;
+};

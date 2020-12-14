38a890ca4b05acb8ffcb3889ed952172b0b88364
peoplesing1832 <68176628+peoplesing1832@users.noreply.github.com>
2020-12-14 08:43:57 +0800

    Update 组合总合III.js

--- 组合总合III.js
+++ 组合总合III.js
@@ -1,1 +1,51 @@
+/**
+ * @param {number} k
+ * @param {number} n
+ * @return {number[][]}
+ */
+var combinationSum3 = function(k, n) {
+    const result = [];
+    const candidates = [];
+    const target = n;
 
+    // 初始化
+    for (let i = 1; i <= 9; i++) {
+        candidates.push(i)
+    }
+
+    const fn = (sum, arr, candidates, k) => {
+        if (k >= 0) {
+            if (k === 0) {
+                if (sum === target) {
+                    result.push(arr)
+                }
+            } else {
+                for (let i = 0; i < candidates.length; i++) {
+                    const temp = candidates.slice(i + 1)
+                    fn(
+                        sum + candidates[i],
+                        [...arr, candidates[i]],
+                        [...temp],
+                        k - 1
+                    );
+                }
+            }
+        }
+    }
+
+    for (let i = 0; i < candidates.length; i++) {
+        if (k === 1 && candidates[i] === target) {
+            result.push([candidates[i]])
+        } else {
+            const temp = candidates.slice(i + 1)
+            fn(
+                candidates[i],
+                [candidates[i]],
+                [...temp],
+                k - 1
+            )
+        }
+    }
+
+    return result;
+};

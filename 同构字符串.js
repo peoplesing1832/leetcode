348b6a611c4c9386bd56fffdc5af062a7714899e
peoplesing1832 <68176628+peoplesing1832@users.noreply.github.com>
2020-12-27 17:10:37 +0800

    Update 同构字符串.js

--- 同构字符串.js
+++ 同构字符串.js
@@ -1,1 +1,29 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+    if (s.length !== t.length) {
+        return false
+    }
+    const sMap = new Map()
+    const tMap = new Map()
+    let len = s.length
+    for (let i = 0; i < s.length; i++) {
+        sMap.set(s[i], t[i])
+        tMap.set(t[i], s[i])
+    }
 
+    while (len) {
+        if (
+            sMap.get(s[len - 1]) !== t[len - 1] ||
+            tMap.get(t[len - 1]) !== s[len - 1]
+        ) {
+            return false
+        }
+        len -= 1
+    }
+
+    return true
+};

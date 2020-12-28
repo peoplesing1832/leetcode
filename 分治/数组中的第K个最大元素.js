01e6e96b1e1b5800f50ffe4c1b9b41e77ac97aed
peoplesing1832 <68176628+peoplesing1832@users.noreply.github.com>
2020-12-28 16:58:23 +0800

    Update 数组中的第K个最大元素.js

--- 分治/数组中的第K个最大元素.js
+++ 分治/数组中的第K个最大元素.js
@@ -1,1 +1,41 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function(nums, k) {
+    let result = null
 
+    const divideAndConquer = (arr, base) => {
+
+        if (arr.length === 1) {
+            result = arr[0]
+            return
+        }
+
+        const referenceValue = arr[0]
+        const min = []
+        const max = []
+        for (let i = 1; i < arr.length; i++) {
+            if (arr[i] > referenceValue) {
+                max.push(arr[i])
+            } else {
+                min.push(arr[i])
+            }
+        }
+        max.push(referenceValue)
+        const maxLen = max.length + base;
+
+        if (maxLen >= k && max.length) {
+            // 说明k存在在max数组中
+            divideAndConquer(max, base)
+        } else if (maxLen < k && min.length) {
+            // 说明k存在在min数组中
+            divideAndConquer(min, maxLen)
+        }
+    }
+
+    divideAndConquer(nums, 0)
+
+    return result
+};

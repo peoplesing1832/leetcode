35090013bf4857a44cddc5db9dc484696ff838eb
peoplesing1832 <68176628+peoplesing1832@users.noreply.github.com>
2020-12-29 23:06:47 +0800

    Update 多数元素.js

--- 分治/多数元素.js
+++ 分治/多数元素.js
@@ -1,17 +1,3 @@
-
-// 给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数 大于 ⌊ n/2 ⌋ 的元素。
-// 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
-
-// 示例 1：
-// 输入：[3,2,3]
-// 输出：3
-
-// 示例 2：
-// 输入：[1, 2, 3, 4, 4, 4]
-// [1, 4, 3, 2, 4, 5] [4, 4, 4]
-// 
-// 输出：2
-
 /**
  * @param {number[]} nums
  * @return {number}
@@ -30,7 +16,7 @@ var majorityElement = function(nums) {
 
     const divideAndConquer = (arr) => {
         if (arr.length === 1) {
-            return arr[0]
+            return null
         }
 
         if (arr.length === 2) {
@@ -65,5 +51,3 @@ var majorityElement = function(nums) {
 
     return divideAndConquer(nums)
 };
-
-majorityElement([6,6,6,7,7])

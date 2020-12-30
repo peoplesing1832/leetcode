b2c0716985b06fac9bb1c97054a536c4b8a2e78a
peoplesing1832 <68176628+peoplesing1832@users.noreply.github.com>
2020-12-30 09:09:39 +0800

    Update 至少有K个重复字符的最长子串.js

--- 分治/至少有K个重复字符的最长子串.js
+++ 分治/至少有K个重复字符的最长子串.js
@@ -1,1 +1,39 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var longestSubstring = function(s, k) {
+    if (s.length < k) {
+        return 0
+    }
 
+    const getSplitDots = (str) => {
+        let splitDots = []
+        const hash = new Map()
+        for (let i = 0; i < str.length; i++) {
+            const key = str[i]
+            if (!hash.has(key)) {
+                hash.set(key, [i])
+            } else {
+                const val = hash.get(key)
+                hash.set(key, [...val, i])
+            }
+        }
+        const values = hash.values()   
+        for (let value of values) {
+            if (value.length < k) {
+                splitDots = [...splitDots, ...value]
+            }
+        }
+        splitDots.sort((a, b) => a - b)
+        return splitDots
+    }
+
+    const divideAndConquer = (str) => {
+        // 切割的点
+        let splitDots = getSplitDots(str)
+    }
+
+    return divideAndConquer(s)
+};

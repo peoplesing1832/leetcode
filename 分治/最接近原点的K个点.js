693bf2568fccee22ac6e18b5524535bc684d5c1a
peoplesing1832 <68176628+peoplesing1832@users.noreply.github.com>
2020-12-31 18:08:45 +0800

    Update 最接近原点的K个点.js

--- 分治/最接近原点的K个点.js
+++ 分治/最接近原点的K个点.js
@@ -1,1 +1,21 @@
-
+```js
+/**
+ * @param {number[][]} points
+ * @param {number} K
+ * @return {number[][]}
+ */
+var kClosest = function(points, K) {
+    
+    // 欧几里得距离
+    const getEuclideanDistance = (o1, o2) => {
+        const [x1, y1] = o1;
+        const [x2, y2] = o2;
+        return Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
+    }
+    
+    const divideAndConquer = (arr) => {
+    }
+    
+    return reuslt;
+};
+```

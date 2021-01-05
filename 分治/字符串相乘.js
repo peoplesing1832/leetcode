c2c115fdab2ef1a5da77b19d0e6696e0ca5d9629
peoplesing1832 <68176628+peoplesing1832@users.noreply.github.com>
2021-01-05 20:08:15 +0800

    Update 字符串相乘.js

--- 分治/字符串相乘.js
+++ 分治/字符串相乘.js
@@ -1,6 +1,17 @@
-var addition = function(num1, num2, num3, num4) {
-    // 暂时的, 之后需要修改为字符串加法
-    return Number(num1) + Number(num2) + Number(num3) + Number(num4)
+var addition = function(num1, num2) {
+    // 加法这里直接拷贝了, leetcode官方的字符串相加的解答
+    let i = num1.length - 1, j = num2.length - 1, add = 0;
+    const ans = [];
+    while (i >= 0 || j >= 0 || add != 0) {
+        const x = i >= 0 ? num1.charAt(i) - '0' : 0;
+        const y = j >= 0 ? num2.charAt(j) - '0' : 0;
+        const result = x + y + add;
+        ans.push(result % 10);
+        add = Math.floor(result / 10);
+        i -= 1;
+        j -= 1;
+    }
+    return ans.reverse().join('');
 }
 
 var padZero = function (num) {
@@ -37,6 +48,7 @@ var multiply = function(num1, num2) {
         } else {
             str1High = str1
             str1Low = '0'
+            str1Carry = 0
         }
 
         if (str2.length > 1) {
@@ -47,40 +59,40 @@ var multiply = function(num1, num2) {
         } else {
             str2High = str2
             str2Low = '0'
+            str2Carry = 0
         }
 
-        
         if (str1High.length <= 1 && str2High.length <= 1) {
             r1 = String(Number(str1High) * Number(str2High)) + padZero(str1Carry + str2Carry)
         } else {
-            // console.log(str1High, str2High)
             r1 = divideAndConquer(str1High, str2High) + padZero(str1Carry + str2Carry)
         }
 
         if (str1High.length <= 1 && str2Low.length <= 1) {
             r2 = String(Number(str1High) * Number(str2Low)) + padZero(str1Carry)
         } else {
-            // console.log(str1High, str2Low)
             r2 = divideAndConquer(str1High, str2Low) + padZero(str1Carry)
         }
 
         if (str1Low.length <= 1 && str2High.length <= 1) {
             r3 = String(Number(str1Low) * Number(str2High)) + padZero(str2Carry)
         } else {
-            // console.log(str1Low, str2High)
             r3 = divideAndConquer(str1Low, str2High) + padZero(str2Carry)
         }
 
         if (str1Low.length <= 1 && str2Low.length <= 1) {
             r4 = String(Number(str1Low) * Number(str2Low)) + ''
         } else {
-            // console.log(str1Low, str1Low)
             r4 = divideAndConquer(str1Low, str2Low)
         }
 
-        return addition(r1, r2, r3, r4)
+        return addition(addition(r1, r2), addition(r3, r4))
     }
 
-    return divideAndConquer(num1, num2)
-};
+    const result =  divideAndConquer(num1, num2)
 
+    if (result[0] === '0') {
+        return '0'
+    }
+    return result
+};

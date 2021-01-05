796cf201c0b9f387342e417eaed15bb08b98c99e
peoplesing1832 <68176628+peoplesing1832@users.noreply.github.com>
2021-01-05 11:31:57 +0800

    Update 为运算表达式设计优先级.js

--- 分治/为运算表达式设计优先级.js
+++ 分治/为运算表达式设计优先级.js
@@ -3,7 +3,6 @@
  * @return {number[]}
  */
 var diffWaysToCompute = function(input) {
-    const hash = new Map()
     const result = []
     const operatorHash = {
         '+': true,
@@ -13,7 +12,6 @@ var diffWaysToCompute = function(input) {
 
     // 获取排列组合
     const getPermutations = (a, b, operator) => {
-       
         const hash = {}
         const result = []
         for (let i = 0; i < a.length; i++) {
@@ -35,24 +33,27 @@ var diffWaysToCompute = function(input) {
                 const right = str.slice(i + 1)
                 const leftRes = []
                 const rightRes = []
-                if (left.length > 1) {
+                if (isNaN(Number(left))) {
                     divideAndConquer(left, leftRes)
                 } else {
                     leftRes.push(left)
                 }
-                if (right.length > 1) {
+                if (isNaN(Number(right))) {
                     divideAndConquer(right, rightRes)
                 } else {
                     rightRes.push(right)
                 }
-                res.concat(getPermutations(leftRes, rightRes, operator))
+                res.push(...getPermutations(leftRes, rightRes, operator))
             }
         }
     }
 
     divideAndConquer(input, result)
 
-    return result;
-};
+    // 如果是纯数字的情况
+    if (result.length === 0) {
+        return [Number(input)]
+    }
 
-diffWaysToCompute("2-1-1")
+    return result.map(item => eval(item));
+};

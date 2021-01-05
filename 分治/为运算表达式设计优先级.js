06f4c27f838d425fb2ee388570b2e00aadedda18
peoplesing1832 <peoplesing1832@gmail.com>
2021-01-05 08:34:06 +0800

    Update: 为运算表达式设计优先级

--- 分治/为运算表达式设计优先级.js
+++ 分治/为运算表达式设计优先级.js
@@ -12,46 +12,47 @@ var diffWaysToCompute = function(input) {
     }
 
     // 获取排列组合
-    const getPermutations = (a, b) => {
+    const getPermutations = (a, b, operator) => {
+       
         const hash = {}
+        const result = []
         for (let i = 0; i < a.length; i++) {
             for (let j = 0; j < b.length; j++) {
-                
+                const key = `((${a[i]})${operator}(${b[j]}))`
+                if (!hash[key]) {
+                    result.push(key)
+                }
             }
         }
+        return result
     }
 
     const divideAndConquer = (str, res) => {
-
         for (let i = 0; i < str.length; i++) {
             const operator = str[i]
-            const left = str.slice(0, i)
-            const right = str.slice(i + 1)
-            const leftRes = []
-            const rightRes = []
-            if (left.length > 1) {
-                divideAndConquer(left, leftRes)
-            } else {
-                leftRes.push(left)
+            if (operatorHash[operator]) {
+                const left = str.slice(0, i)
+                const right = str.slice(i + 1)
+                const leftRes = []
+                const rightRes = []
+                if (left.length > 1) {
+                    divideAndConquer(left, leftRes)
+                } else {
+                    leftRes.push(left)
+                }
+                if (right.length > 1) {
+                    divideAndConquer(right, rightRes)
+                } else {
+                    rightRes.push(right)
+                }
+                res.concat(getPermutations(leftRes, rightRes, operator))
             }
-            if (right.length > 1) {
-                divideAndConquer(right, rightRes)
-            } else {
-                rightRes.push(right)
-            }
-
-
-            // if (operatorHash[operator]) {
-            //     // 把字符串切割两半
-            //     left = 
-            //     right = 
-            //     divideAndConquer(left, leftRes)
-            //     divideAndConquer(left, leftRes)
-            // }
         }
     }
 
-    divideAndConquer(input, [])
+    divideAndConquer(input, result)
 
     return result;
 };
+
+diffWaysToCompute("2-1-1")

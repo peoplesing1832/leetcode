0556b62a427ea551743e09f16fae783d3ca7d75a
peoplesing1832 <68176628+peoplesing1832@users.noreply.github.com>
2021-01-11 23:32:05 +0800

    Update x 的平方根.js

--- 二分/x 的平方根.js
+++ 二分/x 的平方根.js
@@ -1,1 +1,26 @@
+// 二分法
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    let result = null
 
+    const binarySearch = (start, end) => {
+        if (start <= end) {
+            const m = Math.floor(((start + end) / 2))
+            if (m ** 2 === x) {
+                result = m
+            } else if (m ** 2 > x) {
+                binarySearch(start, m - 1)
+            } else {
+                result = result === null ? m : Math.abs(x - (m**2)) < Math.abs(x - (result**2)) ? m : result
+                binarySearch(m + 1, end)
+            }
+        }
+    }
+
+    binarySearch(0, x)
+
+    return result
+};

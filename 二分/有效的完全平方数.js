66ab4c5ecd00ef9979771771a5acd7d4cfc1cfa3
peoplesing1832 <68176628+peoplesing1832@users.noreply.github.com>
2021-01-13 08:26:00 +0800

    Update 有效的完全平方数.js

--- 二分/有效的完全平方数.js
+++ 二分/有效的完全平方数.js
@@ -7,3 +7,36 @@
 var isPerfectSquare = function(num) {
     return num**0.5 % 1 == 0
 };
+
+
+// 二分
+var mySqrt = function(x) {
+    let result = null
+
+    const binarySearch = (start, end) => {
+        if (start <= end) {
+            const m = Math.floor(((start + end) / 2))
+            if (m ** 2 === x) {
+                result = m
+            } else if (m ** 2 > x) {
+                binarySearch(start, m - 1)
+            } else {
+                result = result === null ? m : Math.abs(x - (m**2)) < Math.abs(x - (result**2)) ? m : result
+                binarySearch(m + 1, end)
+            }
+        }
+    }
+
+    binarySearch(0, x)
+
+    return result
+};
+
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare = function(num) {
+    const result = mySqrt(num)
+    return result ** 2 === num
+};

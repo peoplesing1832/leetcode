9ae75fda02f5797aacf56196d9dd165f39f415fc
peoplesing1832 <68176628+peoplesing1832@users.noreply.github.com>
2021-01-18 15:03:02 +0800

    Update Pow(x, n).js

--- Pow(x, n).js
+++ Pow(x, n).js
@@ -1,1 +1,28 @@
 
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function(x, n) {
+    const pow = (x, n) => {
+        if (n === 0) {
+            return 1
+        }
+        if (n === 1) {
+            return x
+        }
+        const num = pow(x, Math.floor(n / 2))
+        if (n & 1) {
+            // 如果是奇数
+            return num * num * x
+        } else {
+            // 如果是偶数
+            return num * num
+        }
+    }
+
+    const result = pow(x, Math.abs(n));
+
+    return n > 0 ? result : (1 / result)
+};

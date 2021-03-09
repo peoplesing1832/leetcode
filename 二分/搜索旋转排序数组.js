38e6aa6bf01a143a63bd6d260bfe81560e60442c
peoplesing1832 <peoplesing1832@gmail.com>
2021-03-09 09:13:03 +0800

    update

--- 二分/搜索旋转排序数组.js
+++ 二分/搜索旋转排序数组.js
@@ -5,5 +5,41 @@
  * @return {number}
  */
 var search = function(nums, target) {
+    let result = -1
 
+    const isOrder = (arr) => {
+        const start = arr[0]
+        const end = arr[arr.length - 1]
+        return start < end
+    }
+
+    const binarySearch = (start, end) => {
+        if (start <= end) {
+            const middIndex = Math.floor(((start + end) / 2))
+            const midd = nums[middIndex]
+            const left = nums.slice(start, middIndex)
+            const right = nums.slice(middIndex + 1)
+            if (midd === target) {
+                result = middIndex
+                return
+            } else {
+                // 如果left有序
+                if (isOrder(left) && (left[0] <= target && left[left.length - 1] >= target)) {
+                    binarySearch(start, middIndex - 1)
+                } else {
+                    binarySearch(middIndex + 1, end)
+                }
+                // 如果right有序
+                if (isOrder(right) && (right[0] <= target && right[right.length - 1] >= target)) {
+                    binarySearch(middIndex + 1, end)
+                } else {
+                    binarySearch(start, middIndex - 1)
+                }
+            }
+        }
+    }
+
+    binarySearch(0, nums.length - 1)
+
+    return result
 };

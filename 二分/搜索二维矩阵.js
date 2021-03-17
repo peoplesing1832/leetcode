3d57744e33ff0a6337b824cb6091d558645f47f3
peoplesing1832 <peoplesing1832@gmail.com>
2021-03-17 23:04:35 +0800

    update

--- 二分/搜索二维矩阵.js
+++ 二分/搜索二维矩阵.js
@@ -61,5 +61,3 @@ var searchMatrix = function(matrix, target) {
 
     return result;
 };
-
-searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3)
--- 二分/有效的完全平方数.js
+++
@@ -1,2 +0,0 @@
-// https://tw.answers.yahoo.com/question/index?qid=20080714000015KK08695&guccounter=1&guce_referrer=aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8&guce_referrer_sig=AQAAAGqD7yNzrIdTl3v3DUWrXPRHE3m_p34DcztGVlJpGsgTmKjZpuJp9aOfv-aAyTpgbJmOltLBT8KbqhHNIUgKnTqXutMjJhW8f4gu6vagfsdulrWJz8SRRGHH48ntHt19jycKdKzBUc13g3sTiw4hWTwH2TusAj8qqf-qMQZKbuS1
-// 简体中文知识圈真的恶臭，没一个靠谱的解释

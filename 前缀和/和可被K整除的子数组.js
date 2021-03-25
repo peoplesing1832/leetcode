a33cff1b01ca930615753616fee3c1495eca49d7
peoplesing1832 <peoplesing1832@gmail.com>
2021-03-25 09:10:52 +0800

    update

--- 前缀和/和可被K整除的子数组.js
+++ 前缀和/和可被K整除的子数组.js
@@ -3,34 +3,29 @@
  * @param {number} K
  * @return {number}
  */
- var subarraysDivByK = function(A, K) {
-    // 前缀和数组
-    const pre = []
+// （pre[i] - pre[j - 1]) % K === 0 =>pre[i] % K === pre[j - 1] % K
+var subarraysDivByK = function(A, K) {
     let count = 0
-
-    // 构建前缀和数组
-    for (let i = 0; i < A.length; i++) {
-        const a = A[i]
-        const b = pre[i - 1] === undefined ? 0 : pre[i - 1]
-        pre[i] = a + b
-    }
+    let preSum = 0
+    let hash = {0: 1}
 
     for (let i = 0; i < A.length; i++) {
-        for (let j = 0; j <= i; j++) {
-            // 计算区间和，查找到满足条件的区间和，count加一
-            let intervalSum;
-            if (j === 0) {
-                intervalSum = pre[i]
-            } else if (j === i) {
-                intervalSum = A[i]
-            } else {
-                intervalSum = pre[i] - pre[j - 1]
-            }
-            if (intervalSum % K === 0) {
-                count += 1
-            }
+        preSum += A[i]
+        let key = preSum % K
+        if (hash[key]) {
+            count += hash[key]
+        }
+        if (key === 0) {
+            count += 1
+        }
+
+        if (!hash[key]) {
+            hash[key] = 1
+        } else {
+            hash[key] += 1
         }
     }
 
     return count
 };
+

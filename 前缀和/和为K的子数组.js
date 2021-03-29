e64d7b115e60cf47740849f66aef013d0f57e795
peoplesing1832 <peoplesing1832@gmail.com>
2021-03-29 20:28:01 +0800

    Merge branch 'master' of github.com:peoplesing1832/leetcode

--- 前缀和/和为K的子数组.js
+++ 前缀和/和为K的子数组.js
@@ -3,22 +3,19 @@
  * @param {number} k
  * @return {number}
  */
- var subarraySum = function(nums, k) {
+var subarraySum = function(nums, k) {
     let count = 0
     let preSum = 0
-    let hash = {}
+    // 记录前缀和出现的次数, 0:1, 是为了排除preSum已经等于k的情况
+    let hash = {0: 1}
 
     for (let i = 0; i < nums.length; i++) {
         preSum += nums[i]
         const key = preSum - k
         if (hash[key]) {
-           count += hash[key]
-        }
-        if (preSum === k) {
-            count += 1
+            count += hash[key]
         }
 
-        // 记录前缀和出现的次数
         if (!hash[preSum]) {
             hash[preSum] = 1
         } else {

20ed17ac3e894337b309dbc14bdf8a71ad39412b
peoplesing1832 <peoplesing1832@gmail.com>
2021-04-17 20:42:01 +0800

    update

--- 二叉树/二叉树的最小深度.js
+++ 二叉树/二叉树的最小深度.js
@@ -10,6 +10,36 @@
  * @param {TreeNode} root
  * @return {number}
  */
- var minDepth = function(root) {
-    
+var minDepth = function(root) {
+    let min = Number.MAX_VALUE
+
+    const traverside = (nodes, depth) => {
+        if (nodes.length) {
+            const childs = []
+            for (let i = 0; i < nodes.length; i += 1) {
+                const node = nodes[i]
+                if (node.left) {
+                    childs.push(node.left)
+                }
+                if (node.right) {
+                    childs.push(node.right)
+                }
+                if (!node.left && !node.right) {
+                    // 没有子节点的节点
+                    min = Math.min(depth, min)
+                    // 终止遍历
+                    return
+                }
+            }
+            traverside(childs, depth + 1)
+        }
+    }
+
+    if (root) {
+        traverside([root], 1)
+    } else {
+        return 0
+    }
+
+    return min
 };
